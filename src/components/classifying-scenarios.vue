a5edaa7d327c26d7e5345e70bb13ad41fb3cbcee
huaHaoFan <qijizaixian_318cx@163.com>
2023-08-04 17:23:04 +0800

    8/4

--- src/components/classifying-scenarios.vue
+++ src/components/classifying-scenarios.vue
@@ -50,7 +50,7 @@ export default {
             dataList: "",
             drawer: false,
             isShowwork: true,
-            isShowsocialize: false,
+            isShowsocialize: true,
             isShowdrawer: "",
             isShowReEdit: false,
             objList: {
